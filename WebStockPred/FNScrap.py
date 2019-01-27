75e4d0661282177c7b1207e1633c22bc33d523d1
Andrew Kiggins <30563055+Kiggins26@users.noreply.github.com>
2019-01-27 06:16:09 -0800

    Update FNScrap.py

--- WebStockPred/FNScrap.py
+++ WebStockPred/FNScrap.py
@@ -1,4 +1,8 @@
 #BeautifulSoup and Requests are needed
+posWords = ['first','better','develop','lets you', 'new','product','love','built','sale','getting','fast','adding','will support','donation',
+'good','good news']
+negWords = ['fraud','fined','left','trouble','nervous',' shutting down','scam','harms','expensive','costly','shrinks','decrease','bad','bad news',
+'awful','alarming','contradictory','dishonorable','dirty','faulty','hostile','malicious','offensive','oppressive']
 import urllib2
 import BeautifulSoup
 import re
@@ -19,12 +23,13 @@ def getPageText(url):
     return Newlines.sub('\n', txt)
 
 def main():
+
     urls = [
-        'https://www.nasdaq.com/symbol/fb',
-        'https://www.nasdaq.com/symbol/aapl',
-        'https://www.nasdaq.com/symbol/amzn',
-        'https://www.nasdaq.com/symbol/nflx',
-        'https://www.nasdaq.com/symbol/GOOGL'
+        'https://www.theverge.com/search?q=facebook',
+        'https://www.theverge.com/search?q=apple',
+        'https://www.theverge.com/search?q=amazon',
+        'https://www.theverge.com/search?q=netflix',
+        'https://www.theverge.com/search?q=google'
     ]
     text_file = open("Data.txt","w")
     
