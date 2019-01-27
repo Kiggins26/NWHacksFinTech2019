6ab3836cd6903ac69b0d805e78a8d74fe931e36f
Andrew Kiggins <30563055+Kiggins26@users.noreply.github.com>
2019-01-27 01:04:26 -0800

    Create Pricegetter.py

---
+++ WebStockPred/Pricegetter.py
@@ -0,0 +1,39 @@
+#BeautifulSoup and Requests are needed
+import urllib2
+import BeautifulSoup
+import re
+
+Newlines = re.compile(r'[\r\n]\s+')
+
+def getPageText(url):
+    # given a url, get page content
+    data = urllib2.urlopen(url).read()
+    # parse as html structured document
+    bs = BeautifulSoup.BeautifulSoup(data, convertEntities=BeautifulSoup.BeautifulSoup.HTML_ENTITIES)
+    # kill javascript content
+    for s in bs.findAll('script'):
+        s.replaceWith('')
+    # find body and extract text
+    txt = bs.find('body').getText('\n')
+    # remove multiple linebreaks and whitespace
+    return Newlines.sub('\n', txt)
+
+def main():
+    urls = [
+        'https://www.nasdaq.com/symbol/fb',
+        'https://www.nasdaq.com/symbol/aapl',
+        'https://www.nasdaq.com/symbol/amzn',
+        'https://www.nasdaq.com/symbol/nflx',
+        'https://www.nasdaq.com/symbol/GOOGL'
+    ]
+    txt = [getPageText(url) for url in urls]
+    for i in txt:
+        print(i)
+        print("\n")
+        print("----")
+        print("\n")
+
+
+
+if __name__=="__main__":
+    main()
